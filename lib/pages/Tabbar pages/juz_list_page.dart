27c1bac55022922f9d6e851c81588e98db6e1a8e
Junaidh haneefa <junaidh.2307@gmail.com>
2024-08-29 13:46:35 +0530

    merge changes from junaidh

---
+++ lib/pages/Tabbar pages/juz_list_page.dart
@@ -0,0 +1,196 @@
+
+import 'package:alquran_web/services/quran_services.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:flutter_svg/svg.dart';
+
+class JuzListPage extends StatefulWidget {
+  JuzListPage({super.key});
+
+  @override
+  _JuzListPageState createState() => _JuzListPageState();
+}
+
+class _JuzListPageState extends State<JuzListPage> {
+  late Future<List<Map<String, dynamic>>> _juzList;
+  final QuranService _quranService = QuranService();
+
+  @override
+  void initState() {
+    super.initState();
+    _juzList = _fetchJuzList();
+  }
+
+  Future<List<Map<String, dynamic>>> _fetchJuzList() async {
+    final surahs = await _quranService.fetchSurahs();
+    final juzList = <Map<String, dynamic>>[];
+
+    int currentJuz = 1;
+    List<Map<String, dynamic>> currentJuzSurahs = [];
+
+    for (final surah in surahs) {
+      if (currentJuzSurahs.length == 0 || currentJuzSurahs.length >= 20) {
+        if (currentJuzSurahs.isNotEmpty) {
+          juzList.add({
+            'juz': currentJuz,
+            'surahs': currentJuzSurahs,
+          });
+        }
+        currentJuz++;
+        currentJuzSurahs = [];
+      }
+      currentJuzSurahs.add(surah);
+    }
+
+    if (currentJuzSurahs.isNotEmpty) {
+      juzList.add({
+        'juz': currentJuz,
+        'surahs': currentJuzSurahs,
+      });
+    }
+
+    return juzList;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    const gridViewColor = Color.fromARGB(255, 244, 244, 244);
+    final screenWidth = MediaQuery.of(context).size.width;
+
+    int crossAxisCount = 3;
+
+    if (screenWidth < 480) {
+      crossAxisCount = 1;
+    } else if (screenWidth < 800) {
+      crossAxisCount = 2;
+    } else {
+      crossAxisCount = 3;
+    }
+
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 32.0),
+        child: FutureBuilder<List<Map<String, dynamic>>>(
+          future: _juzList,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              final juzList = snapshot.data!;
+              return MasonryGridView.count(
+                crossAxisCount: crossAxisCount,
+                itemCount: juzList.length,
+                shrinkWrap: true,
+                itemBuilder: (context, index) {
+                  final juz = juzList[index];
+                  return Card(
+                    margin: const EdgeInsets.all(8),
+                    elevation: 0,
+                    color: gridViewColor,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                'Juz ${juz['juz']}',
+                                style: const TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              TextButton(
+                                  onPressed: () {},
+                                  child: const Text('Read Juz'))
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          height: juz['surahs'].length *
+                              80.0, // Adjust the height based on the number of surahs
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: ListView.builder(
+                              physics: const NeverScrollableScrollPhysics(),
+                              itemCount: juz['surahs'].length,
+                              itemExtent: 70.0,
+                              itemBuilder: (context, surahIndex) {
+                                final surah = juz['surahs'][surahIndex];
+                                return Card(
+                                  elevation: 1,
+                                  color: Colors.white,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                  child: ListTile(
+                                    contentPadding: const EdgeInsets.symmetric(
+                                        horizontal: 16),
+                                    leading: StarNumber(
+                                      number:
+                                          int.parse(surah['SuraId'].toString()),
+                                    ),
+                                    title: Text(
+                                      surah['MSuraName'],
+                                      style: const TextStyle(
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.w600,
+                                        fontSize: 14,
+                                      ),
+                                    ),
+                                    subtitle: Row(
+                                      children: [
+                                        SvgPicture.asset(
+                                          surah['SuraType'] == 'Makkiya'
+                                              ? "icons/Makiyyah_Icon.svg"
+                                              : "icons/Madaniyya_Icon.svg",
+                                          height: 11,
+                                          width: 9,
+                                        ),
+                                        const SizedBox(width: 4),
+                                        Text(
+                                          '${surah['TotalAyas']} Ayat',
+                                          style: const TextStyle(
+                                            fontSize: 12,
+                                            color: Colors.black,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    trailing: Text(
+                                      surah['ASuraName'],
+                                      style: const TextStyle(
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 14,
+                                      ),
+                                    ),
+                                    onTap: () {
+                                      // Navigate to Surah Detailed page with the selected surah
+                                    },
+                                  ),
+                                );
+                              },
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              );
+            } else if (snapshot.hasError) {
+              return Center(
+                child: Text('Error: ${snapshot.error}'),
+              );
+            } else {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
