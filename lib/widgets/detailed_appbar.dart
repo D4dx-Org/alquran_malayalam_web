1c0ebae5d3f6ae2ac5f4a03326c6975db81c620a
Junaidh haneefa <junaidh.2307@gmail.com>
2024-09-02 15:32:44 +0530

    translation page responsiveness and about us page created

--- lib/widgets/detailed_appbar.dart
+++ lib/widgets/detailed_appbar.dart
@@ -1,171 +1,156 @@
-import 'package:alquran_web/widgets/settings_widget.dart';  
-import 'package:alquran_web/widgets/surah_bottom_row.dart';  
-import 'package:flutter/material.dart';  
-import 'package:flutter_svg/svg.dart';  
-import 'package:google_fonts/google_fonts.dart';  
 
-// Enum to represent different pages  
-enum AppPage { detailedsurah, articles }  
+import 'package:alquran_web/widgets/articles_bottom_row.dart';
+import 'package:alquran_web/widgets/settings_widget.dart';
+import 'package:alquran_web/widgets/surah_bottom_row.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class DetailedAppbar extends StatefulWidget implements PreferredSizeWidget {  
-  final AppPage currentPage;  
-  final TabController tabController;  
+// Enum to represent different pages
+enum AppPage { detailedsurah, articles }
 
-  const DetailedAppbar({  
-    super.key,  
-    required this.currentPage,  
-    required this.tabController,  
-  });  
+class DetailedAppbar extends StatefulWidget implements PreferredSizeWidget {
+  final AppPage currentPage;
+  final TabController tabController;
 
-  @override  
-  State<DetailedAppbar> createState() => _DetailedAppbarState();  
+  const DetailedAppbar({
+    super.key,
+    required this.currentPage,
+    required this.tabController,
+  });
 
-  @override  
-  Size get preferredSize =>  
-      const Size.fromHeight(150); // 80 for top row + 50 for bottom row  
-}  
+  @override
+  State<DetailedAppbar> createState() => _DetailedAppbarState();
 
-class _DetailedAppbarState extends State<DetailedAppbar> {  
-  @override  
-  Widget build(BuildContext context) {  
-    final screenWidth = MediaQuery.of(context).size.width;  
+  @override
+  Size get preferredSize =>
+      const Size.fromHeight(150); // 80 for top row + 50 for bottom row
+}
 
-    // Calculate icon sizes based on screen width  
-    final scaleFactor = (screenWidth / 1440).clamp(0.7, 1.0);  
-    final iconScaleFactor = (screenWidth / 1440).clamp(0.9, 1.2);  
-    final menuIconSize = 24.0 * iconScaleFactor;  
-    final settingsIconSize = 24.0 * iconScaleFactor;  
-    final logoSize = 64.0 * scaleFactor;  
+class _DetailedAppbarState extends State<DetailedAppbar> {
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
 
-    return Column(  
-      children: [  
-        Container(  
-          color: const Color.fromRGBO(115, 78, 9, 1),  
-          child: Padding(  
-            padding: EdgeInsets.symmetric(horizontal: 32.0 * scaleFactor),  
-            child: AppBar(  
-              backgroundColor: Colors.transparent,  
-              toolbarHeight: 80 * scaleFactor,  
-              leadingWidth: 60 * scaleFactor,  
-              leading: IconButton(  
-                icon: Icon(  
-                  Icons.menu_sharp,  
-                  size: menuIconSize,  
-                  color: Colors.white,  
-                  weight: 100,  
-                ),  
-                onPressed: () {  
-                  Scaffold.of(context).openDrawer();  
-                },  
-              ),  
-              actions: <Widget>[  
-                IconButton(  
-                  icon: SvgPicture.asset(  
-                    "icons/Settings_Icon.svg",  
-                    height: settingsIconSize,  
-                    width: settingsIconSize,  
-                    colorFilter:  
-                        const ColorFilter.mode(Colors.white, BlendMode.srcIn),  
-                  ),  
-                  onPressed: () {  
-                    showDialog(  
-                      context: context,  
-                      builder: (BuildContext context) {  
-                        return const SettingsWidget();  
-                      },  
-                    );  
-                  },  
-                ),  
-                SizedBox(width: 8 * scaleFactor),  
-              ],  
-              title: LayoutBuilder(  
-                builder: (context, constraints) {  
-                  return Row(  
-                    mainAxisAlignment: MainAxisAlignment.center,  
-                    children: [  
-                      Image.asset(  
-                        'images/AppBar_Icon.png',  
-                        height: logoSize,  
-                        width: logoSize,  
-                        color: Colors.white,  
-                        fit: BoxFit.contain,  
-                      ),  
-                      SizedBox(width: 8 * scaleFactor),  
-                      Flexible(  
-                        child: Column(  
-                          crossAxisAlignment: CrossAxisAlignment.start,  
-                          mainAxisSize: MainAxisSize.min,  
-                          children: [  
-                            FittedBox(  
-                              fit: BoxFit.scaleDown,  
-                              child: Text(  
-                                "അല്‍-ഖുര്‍ആന്‍",  
-                                style: GoogleFonts.anekMalayalam(  
-                                  fontSize: 35 * scaleFactor,  
-                                  fontWeight: FontWeight.w800,  
-                                  color: Colors.white,  
-                                ),  
-                              ),  
-                            ),  
-                            FittedBox(  
-                              fit: BoxFit.scaleDown,  
-                              child: Text(  
-                                "വാക്കര്‍ത്ഥത്തോടുകൂടിയ പരിഭാഷ",  
-                                style: GoogleFonts.anekMalayalam(  
-                                  color: Colors.white,  
-                                  fontSize: 18 * scaleFactor,  
-                                  fontWeight: FontWeight.w300,  
-                                ),  
-                              ),  
-                            ),  
-                          ],  
-                        ),  
-                      ),  
-                    ],  
-                  );  
-                },  
-              ),  
-              centerTitle: true,  
-            ),  
-          ),  
-        ),  
-        Container(  
-          color: const Color.fromRGBO(115, 78, 9, 1),  
-          child: Padding(  
-            padding: EdgeInsets.symmetric(  
-                horizontal: 32 * scaleFactor, vertical: 8 * scaleFactor),  
-            child: _buildBottomRow(context, scaleFactor, widget.tabController),  
-          ),  
-        ),  
-      ],  
-    );  
-  }  
+    // Calculate icon sizes based on screen width
+    final scaleFactor = (screenWidth / 1440).clamp(0.7, 1.0);
+    final iconScaleFactor = (screenWidth / 1440).clamp(0.9, 1.2);
+    final menuIconSize = 24.0 * iconScaleFactor;
+    final settingsIconSize = 24.0 * iconScaleFactor;
+    final logoSize = 64.0 * scaleFactor;
 
-  Widget _buildBottomRow(BuildContext context, double scaleFactor, TabController tabController) {  
-    switch (widget.currentPage) {  
-      case AppPage.detailedsurah:  
-        return SurahBottomRow(scaleFactor, tabController: tabController);  
-      case AppPage.articles:  
-        return _buildarticlesBottomRow(context, scaleFactor);  
-      default:  
-        return SizedBox.shrink();  
-    }  
-  }  
+    return Column(
+      children: [
+        Container(
+          color: const Color.fromRGBO(115, 78, 9, 1),
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 32.0 * scaleFactor),
+            child: AppBar(
+              backgroundColor: Colors.transparent,
+              toolbarHeight: 80 * scaleFactor,
+              leadingWidth: 60 * scaleFactor,
+              leading: IconButton(
+                icon: Icon(
+                  Icons.menu_sharp,
+                  size: menuIconSize,
+                  color: Colors.white,
+                  weight: 100,
+                ),
+                onPressed: () {
+                  Scaffold.of(context).openDrawer();
+                },
+              ),
+              actions: <Widget>[
+                IconButton(
+                  icon: SvgPicture.asset(
+                    "icons/Settings_Icon.svg",
+                    height: settingsIconSize,
+                    width: settingsIconSize,
+                    colorFilter:
+                        const ColorFilter.mode(Colors.white, BlendMode.srcIn),
+                  ),
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return const SettingsWidget();
+                      },
+                    );
+                  },
+                ),
+                SizedBox(width: 8 * scaleFactor),
+              ],
+              title: LayoutBuilder(
+                builder: (context, constraints) {
+                  return Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        'images/AppBar_Icon.png',
+                        height: logoSize,
+                        width: logoSize,
+                        color: Colors.white,
+                        fit: BoxFit.contain,
+                      ),
+                      SizedBox(width: 8 * scaleFactor),
+                      Flexible(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            FittedBox(
+                              fit: BoxFit.scaleDown,
+                              child: Text(
+                                "അല്‍-ഖുര്‍ആന്‍",
+                                style: GoogleFonts.anekMalayalam(
+                                  fontSize: 35 * scaleFactor,
+                                  fontWeight: FontWeight.w800,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                            FittedBox(
+                              fit: BoxFit.scaleDown,
+                              child: Text(
+                                "വാക്കര്‍ത്ഥത്തോടുകൂടിയ പരിഭാഷ",
+                                style: GoogleFonts.anekMalayalam(
+                                  color: Colors.white,
+                                  fontSize: 18 * scaleFactor,
+                                  fontWeight: FontWeight.w300,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  );
+                },
+              ),
+              centerTitle: true,
+            ),
+          ),
+        ),
+        Container(
+          color: const Color.fromRGBO(115, 78, 9, 1),
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+                horizontal: 32 * scaleFactor, vertical: 8 * scaleFactor),
+            child: _buildBottomRow(context, scaleFactor, widget.tabController),
+          ),
+        ),
+      ],
+    );
+  }
 
-  Widget _buildarticlesBottomRow(BuildContext context, double scaleFactor) {  
-    return Container(  
-      color: Colors.brown[100],  
-      height: 50 * scaleFactor,  
-      child: Padding(  
-        padding: EdgeInsets.symmetric(horizontal: 16.0 * scaleFactor),  
-        child: TextField(  
-          decoration: InputDecoration(  
-            hintText: 'Search...',  
-            border: InputBorder.none,  
-            icon: Icon(Icons.search, size: 24 * scaleFactor),  
-          ),  
-        ),  
-      ),  
-    );  
-  }  
+  Widget _buildBottomRow(BuildContext context, double scaleFactor, TabController tabController) {
+    switch (widget.currentPage) {
+      case AppPage.detailedsurah:
+        return SurahBottomRow(scaleFactor, tabController: tabController);
+      case AppPage.articles:
+        return ArticlesBottomRow(context, scaleFactor, tabController: tabController);
+      default:
+        return SizedBox.shrink();
+    }
+  }
 }
