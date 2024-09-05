03c0263ce5d7bfb3fe8b8ae2cff6da2eb5523d1f
Junaidh haneefa <junaidh.2307@gmail.com>
2024-09-05 06:58:17 +0530

    leading icon corrected in index appbar

--- lib/services/utils.dart
+++ lib/services/utils.dart
@@ -1,10 +1,10 @@
 import 'dart:convert';
 import 'package:flutter/services.dart' show rootBundle;
 
-
-class JuzJsonParser {  
-  Future<Map<String, dynamic>> loadJsonData() async {  
-    final jsonString = await rootBundle.loadString('assets/juz-to-chapter-verse-mappings.json');  
-    return json.decode(jsonString);  
-  }  
-} 
+class JuzJsonParser {
+  Future<Map<String, dynamic>> loadJsonData() async {
+    final jsonString = await rootBundle
+        .loadString('assets/juz-to-chapter-verse-mappings.json');
+    return json.decode(jsonString);
+  }
+}
--- lib/widgets/index_appbar.dart
+++ lib/widgets/index_appbar.dart
@@ -18,98 +18,96 @@ class IndexAppbar extends StatelessWidget implements PreferredSizeWidget {
     final settingsIconSize = 24.0 * iconScaleFactor;
     final logoSize = 64.0 * scaleFactor;
 
-    return Scaffold(
-      extendBodyBehindAppBar: true,
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        toolbarHeight: 80 * scaleFactor,
-        leadingWidth: 60 * scaleFactor,
-        leading: Padding(
+    return AppBar(
+      backgroundColor: Colors.transparent,
+      automaticallyImplyLeading: false,
+      toolbarHeight: 80 * scaleFactor,
+      leadingWidth: 100 * scaleFactor,
+      leading: Padding(
+        padding: EdgeInsets.only(
+          left: screenWidth > 800 ? 50 * scaleFactor : 0.0,
+        ),
+        child: IconButton(
+          icon: Icon(
+            Icons.menu_sharp,
+            size: menuIconSize,
+            color: const Color.fromRGBO(130, 130, 130, 1),
+            weight: 300,
+          ),
+          onPressed: () {
+            Scaffold.of(context).openDrawer();
+          },
+        ),
+      ),
+      actions: <Widget>[
+        Padding(
           padding: EdgeInsets.only(
-            left: screenWidth > 800 ? 50 * scaleFactor : 0.0,
+            right: screenWidth > 800 ? 50 * scaleFactor : 0.0,
           ),
           child: IconButton(
-            icon: Icon(
-              Icons.menu_sharp,
-              size: menuIconSize,
-              color: const Color.fromRGBO(130, 130, 130, 1),
-              weight: 100,
+            icon: SvgPicture.asset(
+              "icons/Settings_Icon.svg",
+              height: settingsIconSize,
+              width: settingsIconSize,
             ),
             onPressed: () {
-              Scaffold.of(context).openDrawer();
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return const SettingsWidget();
+                },
+              );
             },
           ),
         ),
-        actions: <Widget>[
-          Padding(
-            padding: EdgeInsets.only(
-              right: screenWidth > 800 ? 50 * scaleFactor : 0.0,
-            ),
-            child: IconButton(
-              icon: SvgPicture.asset(
-                "icons/Settings_Icon.svg",
-                height: settingsIconSize,
-                width: settingsIconSize,
+      ],
+      title: LayoutBuilder(
+        builder: (context, constraints) {
+          return Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'images/AppBar_Icon.png',
+                height: logoSize,
+                width: logoSize,
+                fit: BoxFit.contain,
               ),
-              onPressed: () {
-                showDialog(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return const SettingsWidget();
-                  },
-                );
-              },
-            ),
-          ),
-        ],
-        title: LayoutBuilder(
-          builder: (context, constraints) {
-            return Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset(
-                  'images/AppBar_Icon.png',
-                  height: logoSize,
-                  width: logoSize,
-                  fit: BoxFit.contain,
-                ),
-                SizedBox(width: 8 * scaleFactor),
-                Flexible(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      FittedBox(
-                        fit: BoxFit.scaleDown,
-                        child: Text(
-                          "അല്‍-ഖുര്‍ആന്‍",
-                          style: GoogleFonts.anekMalayalam(
-                              fontSize: 35 * scaleFactor,
-                              fontWeight: FontWeight.w900,
-                              color: const Color.fromRGBO(115, 78, 9, 1)),
+              SizedBox(width: 8 * scaleFactor),
+              Flexible(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    FittedBox(
+                      fit: BoxFit.scaleDown,
+                      child: Text(
+                        "അല്‍-ഖുര്‍ആന്‍",
+                        style: GoogleFonts.anekMalayalam(
+                            fontSize: 35 * scaleFactor,
+                            fontWeight: FontWeight.w900,
+                            color: const Color.fromRGBO(115, 78, 9, 1)),
+                      ),
+                    ),
+                    FittedBox(
+                      fit: BoxFit.scaleDown,
+                      child: Text(
+                        "വാക്കര്‍ത്ഥത്തോടുകൂടിയ പരിഭാഷ",
+                        style: GoogleFonts.anekMalayalam(
+                          color: const Color.fromRGBO(74, 74, 74, 1),
+                          fontSize: 18 * scaleFactor,
+                          fontWeight: FontWeight.normal,
                         ),
                       ),
-                      FittedBox(
-                        fit: BoxFit.scaleDown,
-                        child: Text(
-                          "വാക്കര്‍ത്ഥത്തോടുകൂടിയ പരിഭാഷ",
-                          style: GoogleFonts.anekMalayalam(
-                            color: const Color.fromRGBO(74, 74, 74, 1),
-                            fontSize: 18 * scaleFactor,
-                            fontWeight: FontWeight.normal,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-              ],
-            );
-          },
-        ),
-        centerTitle: true,
-        scrolledUnderElevation: 0.0,
+              ),
+            ],
+          );
+        },
       ),
+      centerTitle: true,
+      scrolledUnderElevation: 0.0,
     );
   }
 
