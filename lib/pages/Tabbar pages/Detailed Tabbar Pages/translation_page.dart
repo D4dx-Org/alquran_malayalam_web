85047a4f457320e5149826768f1f94c29be29269
Junaidh haneefa <junaidh.2307@gmail.com>
2024-09-09 19:20:43 +0530

    share function  with copy to clipboard also

--- lib/pages/Tabbar pages/Detailed Tabbar Pages/translation_page.dart
+++ lib/pages/Tabbar pages/Detailed Tabbar Pages/translation_page.dart
@@ -1,16 +1,299 @@
-import 'package:alquran_web/controllers/audio_controller.dart';
-import 'package:alquran_web/controllers/bookmarks_controller.dart';
-import 'package:alquran_web/widgets/audio_player_widget.dart';
+// import 'package:alquran_web/controllers/audio_controller.dart';
+// import 'package:alquran_web/controllers/bookmarks_controller.dart';
+// import 'package:alquran_web/widgets/audio_player_widget.dart';
+// import 'package:flutter/material.dart';
+// import 'package:get/get.dart';
+// import 'package:alquran_web/controllers/quran_controller.dart';
+// import 'package:alquran_web/controllers/settings_controller.dart';
+// import 'package:alquran_web/widgets/ayah_action_bar.dart';
+// import 'package:google_fonts/google_fonts.dart';
+// import 'package:share_plus/share_plus.dart';
+
+// class TranslationPage extends StatefulWidget {
+//   const TranslationPage({super.key});
+
+//   @override
+//   State<TranslationPage> createState() => _TranslationPageState();
+// }
+
+// class _TranslationPageState extends State<TranslationPage> {
+//   final _quranController = Get.find<QuranController>();
+//   final _settingsController = Get.find<SettingsController>();
+//   final _bookmarkController = Get.find<BookmarkController>();
+//   final _audioController = Get.find<AudioController>();
+//   final _scrollController = ScrollController();
+//   bool _isLoading = false;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     _scrollController.addListener(_onScroll);
+//   }
+
+//   @override
+//   void dispose() {
+//     _scrollController.removeListener(_onScroll);
+//     _scrollController.dispose();
+//     super.dispose();
+//   }
+
+//   void _onScroll() {
+//     if (!_isLoading &&
+//         _scrollController.position.pixels >=
+//             _scrollController.position.maxScrollExtent) {
+//       _loadMoreAyahLines();
+//     }
+//   }
+
+//   Future<void> _loadMoreAyahLines() async {
+//     if (_isEndOfSurah()) return;
+
+//     setState(() => _isLoading = true);
+//     await _quranController.fetchMoreAyahLines();
+//     setState(() => _isLoading = false);
+//   }
+
+//   bool _isEndOfSurah() {
+//     return _quranController.ayahLines.isNotEmpty &&
+//         _quranController.ayahLines.last['AyaNo'] ==
+//             _quranController.selectedSurahAyahCount.toString();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Column(
+//         children: [
+//           Expanded(
+//             child: Padding(
+//               padding: const EdgeInsets.symmetric(horizontal: 32.0),
+//               child: Obx(
+//                 () => ListView.builder(
+//                   controller: _scrollController,
+//                   itemCount: _quranController.ayahLines.length + 3,
+//                   itemBuilder: (context, index) {
+//                     if (index == 0) {
+//                       return _buildHeader();
+//                     } else if (index == _quranController.ayahLines.length + 1) {
+//                       return _isEndOfSurah()
+//                           ? _buildEndOfSurahMessage()
+//                           : const SizedBox.shrink();
+//                     } else if (index == _quranController.ayahLines.length + 2) {
+//                       return _isLoading
+//                           ? const Center(child: CircularProgressIndicator())
+//                           : const SizedBox.shrink();
+//                     } else {
+//                       return _buildAyah(_quranController.ayahLines[index - 1]);
+//                     }
+//                   },
+//                 ),
+//               ),
+//             ),
+//           ),
+//           AudioPlayerWidget(),
+//         ],
+//       ),
+//     );
+//   }
+
+//   Widget _buildHeader() {
+//     return Column(
+//       children: [
+//         Padding(
+//           padding: const EdgeInsets.all(16.0),
+//           child: Center(
+//             child: Text(
+//               _quranController
+//                   .getArabicSurahName(_quranController.selectedSurahId),
+//               style:
+//                   GoogleFonts.amiri(fontSize: 24, fontWeight: FontWeight.bold),
+//             ),
+//           ),
+//         ),
+//         Padding(
+//           padding: const EdgeInsets.symmetric(horizontal: 16.0),
+//           child: Row(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: [
+//               Text(
+//                 _quranController.selectedSurah,
+//                 style: const TextStyle(fontSize: 18),
+//               ),
+//               const SizedBox(width: 10),
+//               Text(
+//                 '(${_quranController.getSurahMalMean(_quranController.selectedSurahId)})',
+//                 style:
+//                     const TextStyle(fontSize: 16, fontStyle: FontStyle.italic),
+//               ),
+//             ],
+//           ),
+//         ),
+//         const SizedBox(height: 20),
+//       ],
+//     );
+//   }
+
+//   Widget _buildAyah(Map<String, dynamic> ayah) {
+//     int ayahNumber = int.tryParse(ayah['AyaNo'] ?? '') ?? 0;
+//     String lineId = ayah['LineId'] ?? '';
+//     String verseKey = "${_quranController.selectedSurahId}:$ayahNumber";
+
+//     return Column(
+//       crossAxisAlignment: CrossAxisAlignment.end,
+//       children: [
+//         Row(
+//           crossAxisAlignment: CrossAxisAlignment.start,
+//           children: [
+//             Expanded(
+//               child: Column(
+//                 crossAxisAlignment: CrossAxisAlignment.start,
+//                 children: [
+//                   Padding(
+//                     padding: const EdgeInsets.all(16.0),
+//                     child: Obx(() => AyahActionBar(
+//                           ayahNumber: ayahNumber,
+//                           lineId: lineId,
+//                           onPlayPressed: () {
+//                             _audioController.playAyah(verseKey);
+//                           },
+//                           onBookmarkPressed: () {
+//                             _bookmarkController.toggleBookmark(
+//                               _quranController.selectedSurahId,
+//                               ayahNumber,
+//                               lineId,
+//                             );
+//                           },
+//                           onSharePressed: (String shareText) {
+//                             Share.share(shareText);
+//                           },
+//                           isBookmarked: _bookmarkController.isAyahBookmarked(
+//                             _quranController.selectedSurahId,
+//                             ayahNumber,
+//                             lineId,
+//                           ),
+//                           lineWords: ayah['LineWords'],
+//                           translation: ayah['MalTran'],
+//                         )),
+//                   ),
+//                   Row(
+//                     mainAxisAlignment: MainAxisAlignment.end,
+//                     children: [
+//                       Expanded(
+//                         child: Wrap(
+//                           alignment: WrapAlignment.end,
+//                           children: [
+//                             ...(ayah['LineWords'] as List<Map<String, dynamic>>)
+//                                 .reversed
+//                                 .map(
+//                                   (word) => Padding(
+//                                     padding: const EdgeInsets.symmetric(
+//                                         horizontal: 8.0),
+//                                     child: Column(
+//                                       crossAxisAlignment:
+//                                           CrossAxisAlignment.end,
+//                                       children: [
+//                                         _buildArabicWord(word['ArabWord']),
+//                                         _buildTranslation(word['MalWord']),
+//                                       ],
+//                                     ),
+//                                   ),
+//                                 ),
+//                           ],
+//                         ),
+//                       ),
+//                     ],
+//                   ),
+//                   Padding(
+//                     padding: const EdgeInsets.all(16.0),
+//                     child: Obx(
+//                       () => Text(
+//                         ayah['MalTran'],
+//                         style: TextStyle(
+//                           fontSize:
+//                               _settingsController.translationFontSize.value,
+//                         ),
+//                       ),
+//                     ),
+//                   ),
+//                 ],
+//               ),
+//             ),
+//           ],
+//         ),
+//         const Divider(
+//           color: Color.fromRGBO(230, 230, 230, 1),
+//           thickness: 1,
+//           height: 32,
+//         ),
+//       ],
+//     );
+//   }
+
+//   Widget _buildArabicWord(String word) {
+//     return Container(
+//       padding: const EdgeInsets.all(8),
+//       decoration: BoxDecoration(
+//         border: Border.all(
+//           color: const Color.fromRGBO(230, 230, 230, 1),
+//         ),
+//         color: const Color.fromRGBO(249, 249, 249, 1),
+//         borderRadius: BorderRadius.circular(8),
+//       ),
+//       child: Obx(
+//         () => Text(
+//           word,
+//           style: GoogleFonts.amiri(
+//             fontSize: _settingsController.quranFontSize.value,
+//             fontWeight: FontWeight.bold,
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+
+//   Widget _buildTranslation(String translation) {
+//     return Padding(
+//       padding: const EdgeInsets.symmetric(vertical: 4.0),
+//       child: Obx(
+//         () => Text(
+//           translation,
+//           style: TextStyle(
+//             fontSize: _settingsController.translationFontSize.value,
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+
+//   Widget _buildEndOfSurahMessage() {
+//     return const Padding(
+//       padding: EdgeInsets.symmetric(vertical: 16.0),
+//       child: Center(
+//         child: Text(
+//           "End of Surah",
+//           style: TextStyle(
+//             fontSize: 15,
+//             fontWeight: FontWeight.normal,
+//             color: Colors.black,
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:alquran_web/controllers/audio_controller.dart';
+import 'package:alquran_web/controllers/bookmarks_controller.dart';
 import 'package:alquran_web/controllers/quran_controller.dart';
 import 'package:alquran_web/controllers/settings_controller.dart';
+import 'package:alquran_web/widgets/audio_player_widget.dart';
 import 'package:alquran_web/widgets/ayah_action_bar.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:share_plus/share_plus.dart';
 
 class TranslationPage extends StatefulWidget {
-  const TranslationPage({super.key});
+  const TranslationPage({Key? key}) : super(key: key);
 
   @override
   State<TranslationPage> createState() => _TranslationPageState();
@@ -162,9 +445,6 @@ class _TranslationPageState extends State<TranslationPage> {
                               ayahNumber,
                               lineId,
                             );
-                          },
-                          onSharePressed: (String shareText) {
-                            Share.share(shareText);
                           },
                           isBookmarked: _bookmarkController.isAyahBookmarked(
                             _quranController.selectedSurahId,
--- lib/widgets/ayah_action_bar.dart
+++ lib/widgets/ayah_action_bar.dart
@@ -1,7 +1,118 @@
 
+// import 'package:flutter/material.dart';
+// import 'package:flutter_svg/flutter_svg.dart';
+// import 'package:get/get.dart';
+// import 'package:alquran_web/controllers/audio_controller.dart';
+// import 'package:alquran_web/controllers/quran_controller.dart';
+
+// class AyahActionBar extends StatelessWidget {
+//   final int ayahNumber;
+//   final String lineId;
+//   final VoidCallback onPlayPressed;
+//   final VoidCallback onBookmarkPressed;
+//   final Function(String) onSharePressed;
+//   final bool isBookmarked;
+//   final List<Map<String, dynamic>> lineWords;
+//   final String translation;
+
+//   AyahActionBar({
+//     Key? key,
+//     required this.ayahNumber,
+//     required this.lineId,
+//     required this.onPlayPressed,
+//     required this.onBookmarkPressed,
+//     required this.onSharePressed,
+//     required this.isBookmarked,
+//     required this.lineWords,
+//     required this.translation,
+//   }) : super(key: key);
+
+//   final AudioController audioController = Get.find<AudioController>();
+//   final QuranController quranController = Get.find<QuranController>();
+
+//   void _handlePlayPressed() {
+//     audioController.showPlayer();
+//     onPlayPressed();
+//   }
+
+//   void _handleSharePressed() {
+//     final arabicWords = lineWords.map((word) => word['ArabWord']).join(' ');
+//     final wordMeanings = lineWords.map((word) => word['MalWord']).join(' ');
+//     final shareText = 'Surah ${quranController.selectedSurah}, Ayah $ayahNumber\n\n'
+//         'Arabic: $arabicWords\n\n'
+//         'Word Meanings: $wordMeanings\n\n'
+//         'Translation: $translation';
+//     onSharePressed(shareText);
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+//       decoration: BoxDecoration(
+//         color: const Color(0xFFF6F6F6),
+//         borderRadius: BorderRadius.circular(8),
+//       ),
+//       child: Row(
+//         mainAxisSize: MainAxisSize.min,
+//         children: [
+//           Text(
+//             '${quranController.selectedSurahId}:$ayahNumber',
+//             style: const TextStyle(
+//               fontSize: 14,
+//               fontWeight: FontWeight.w600,
+//               color: Color(0xFF734E09),
+//             ),
+//           ),
+//           const SizedBox(width: 16),
+//           _buildSvgIconButton('icons/PlayAyah_Icon.svg', _handlePlayPressed),
+//           const SizedBox(width: 16),
+//           _buildIconButton(
+//             isBookmarked ? Icons.bookmark : Icons.bookmark_border,
+//             onBookmarkPressed,
+//             isBookmarked,
+//           ),
+//           const SizedBox(width: 16),
+//           _buildSvgIconButton('icons/ShareAyah_Icon.svg', _handleSharePressed),
+//         ],
+//       ),
+//     );
+//   }
+
+//   Widget _buildSvgIconButton(String assetName, VoidCallback onPressed) {
+//     return GestureDetector(
+//       onTap: onPressed,
+//       child: SvgPicture.asset(
+//         assetName,
+//         height: 14,
+//         width: 14,
+//         colorFilter: const ColorFilter.mode(
+//           Color(0xFF734E09),
+//           BlendMode.srcIn,
+//         ),
+//       ),
+//     );
+//   }
+
+//   Widget _buildIconButton(
+//       IconData icon, VoidCallback onPressed, bool isActive) {
+//     return GestureDetector(
+//       onTap: onPressed,
+//       child: Icon(
+//         icon,
+//         size: 18,
+//         color: const Color(0xFF734E09),
+//       ),
+//     );
+//   }
+// }
+
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
+import 'package:share_plus/share_plus.dart';
 import 'package:alquran_web/controllers/audio_controller.dart';
 import 'package:alquran_web/controllers/quran_controller.dart';
 
@@ -10,7 +121,6 @@ class AyahActionBar extends StatelessWidget {
   final String lineId;
   final VoidCallback onPlayPressed;
   final VoidCallback onBookmarkPressed;
-  final Function(String) onSharePressed;
   final bool isBookmarked;
   final List<Map<String, dynamic>> lineWords;
   final String translation;
@@ -21,7 +131,6 @@ class AyahActionBar extends StatelessWidget {
     required this.lineId,
     required this.onPlayPressed,
     required this.onBookmarkPressed,
-    required this.onSharePressed,
     required this.isBookmarked,
     required this.lineWords,
     required this.translation,
@@ -35,14 +144,46 @@ class AyahActionBar extends StatelessWidget {
     onPlayPressed();
   }
 
-  void _handleSharePressed() {
+  void _handleSharePressed(BuildContext context) {
     final arabicWords = lineWords.map((word) => word['ArabWord']).join(' ');
     final wordMeanings = lineWords.map((word) => word['MalWord']).join(' ');
     final shareText = 'Surah ${quranController.selectedSurah}, Ayah $ayahNumber\n\n'
         'Arabic: $arabicWords\n\n'
         'Word Meanings: $wordMeanings\n\n'
         'Translation: $translation';
-    onSharePressed(shareText);
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Share Options'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ListTile(
+                leading: Icon(Icons.share),
+                title: Text('Share'),
+                onTap: () {
+                  Navigator.pop(context);
+                  Share.share(shareText);
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.content_copy),
+                title: Text('Copy to Clipboard'),
+                onTap: () {
+                  Clipboard.setData(ClipboardData(text: shareText));
+                  Navigator.pop(context);
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('Copied to clipboard')),
+                  );
+                },
+              ),
+            ],
+          ),
+        );
+      },
+    );
   }
 
   @override
@@ -73,7 +214,7 @@ class AyahActionBar extends StatelessWidget {
             isBookmarked,
           ),
           const SizedBox(width: 16),
-          _buildSvgIconButton('icons/ShareAyah_Icon.svg', _handleSharePressed),
+          _buildSvgIconButton('icons/ShareAyah_Icon.svg', () => _handleSharePressed(context)),
         ],
       ),
     );
